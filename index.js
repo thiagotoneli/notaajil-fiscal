47617bcb592fd53827b9e97bea84baee04006838
Thiago Toneli <toneli.suporte@gmail.com>
2026-03-18 14:34:26 -0300

    criacao do encerramento, cancelamento, consulta e geracao do pdf

--- index.js
+++ index.js
@@ -3,9 +3,15 @@
 const nodeNfeNfce = require("node-nfe-nfce");
 const { emitirMDFe } = require("./lib/mdfe/emitir-mdfe");
 const { encerrarMDFe } = require("./lib/mdfe/encerrar-mdfe");
+const { consultarMDFe } = require("./lib/mdfe/consultar-mdfe");
+const { cancelarMDFe } = require("./lib/mdfe/cancelar-mdfe");
+const { gerarMDFePDF } = require("./lib/mdfe/gerar-mdfe-pdf");
 
 module.exports = {
   ...nodeNfeNfce,
   emitirMDFe,
-  encerrarMDFe
+  encerrarMDFe,
+  consultarMDFe,
+  cancelarMDFe,
+  gerarMDFePDF
 };
